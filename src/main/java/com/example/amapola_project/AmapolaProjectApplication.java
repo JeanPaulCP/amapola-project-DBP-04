2e8184fcc9f2b6dbe97b845ed2eaa2103bece055
Paul <jean.cuzcano@utec.edu.pe>
2023-10-24 14:43:00 -0500

    Primeros archivos

---
+++ src/main/java/com/example/amapola_project/AmapolaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.amapola_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmapolaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmapolaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/amapola_project/AmapolaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amapola_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmapolaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
